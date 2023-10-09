5aa271dece2d8e3fce790707458341467536101d
frank chege <francischege602@gmail.com>
2023-10-10 01:40:09 +0300

    py lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    x = len(matrix)
-    # x1 = len(matrix)
-    for y in range(x):
-    # for z in range(x1 - 1):
-       print("{}".format(matrix[y]))
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
