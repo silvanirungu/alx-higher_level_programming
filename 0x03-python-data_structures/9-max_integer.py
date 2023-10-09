86cb3ef4658d2f194433f24420eb1f8fec260872
frank chege <francischege602@gmail.com>
2023-10-10 01:03:03 +0300

    py lists

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == None:
+        return None
+    else:
+        x = len(my_list) - 1
+        maxim = 0
+        for y in range(x):
+            if my_list[y] > maxim:
+                maxim = my_list[y]
+        return maxim
