5994a6290a43babd3cee58b3bc50100e9c1725f5
frank chege <francischege602@gmail.com>
2023-10-12 00:55:24 +0300

    more structs

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_m = []
-    square = lambda x : x**2
     for x in matrix:
-        result = list(map(square, x))
+        result = list(map((lambda x: x**2), x))
         new_m.append(result)
     return new_m
