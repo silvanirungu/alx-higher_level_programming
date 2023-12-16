6dcf876e667a34828aee45def2eab2edb99d8ff8
frank chege <francischege602@gmail.com>
2023-12-16 21:12:33 +0300

    sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,8 +1,10 @@
--- new table
+-- insert new values
 INSERT INTO second_table (score, name)
    VALUES (18, 'Aria'), (12, 'Aria');
+-- delete id 4
 DELETE FROM second_table
     WHERE name = 'George';
+-- show results
 SELECT score, name
     FROM second_table
     ORDER BY score DESC;
