8bf5a2aa1c096d8b2d851a2147b0e11b1e1063f5
frank chege <francischege602@gmail.com>
2023-12-19 16:28:07 +0300

    more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- create user and grant privileges
 CREATE USER IF NOT EXISTS 'user_0d_1@localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT
+GRANT ALL PRIVILEGES
 ON *.*
-TO 'user_0d_1@localhost';
+TO 'user_0d_1@localhost' WITH GRANT OPTION
