1503c27e9725b38f3040b4437113fe35e319f063
frank chege <francischege602@gmail.com>
2023-12-19 16:57:49 +0300

    more queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,7 @@
 -- create database and user and grant permissions
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
 ON hbtn_0d_2.*
 TO 'user_0d_2'@'localhost';
