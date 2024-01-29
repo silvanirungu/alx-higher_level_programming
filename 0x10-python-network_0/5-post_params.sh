1dccf5588cdefb1c24d51400bff6ff8f975a2f16
frank chege <francischege602@gmail.com>
2024-01-30 00:46:51 +0300

    python web

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post info using curl
-curl -s -X  POST -d 'email=test@gamil.com&subject=I will always be here for PLD' "$1"
+curl -s -X  POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
