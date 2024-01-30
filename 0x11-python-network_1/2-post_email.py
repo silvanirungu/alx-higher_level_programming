a0851973412b44943e9cdd0010dbaa86171cc029
frank chege <francischege602@gmail.com>
2024-01-31 01:06:40 +0300

    web server

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,10 @@ send a post request in a url
 import sys
 import urllib.request
 if __name__ == "__main__" :
-    data = 'email='+sys.argv[2]
-    url = sys.argv[1]+'?'+data
-    url = urllib.request.Request(url)
+    data = 'email=' + sys.argv[2]
+    #url = sys.argv[1]+'?'+data
+    data = data.encode('ascii')
+    url = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print('Your email is: {}'.format(body.decode('utf-8')))
