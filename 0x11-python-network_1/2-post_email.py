595b824677d6824963b716abd2d4597c62468438
frank chege <francischege602@gmail.com>
2024-01-31 00:31:25 +0300

    web server

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+send a post request in a url
+"""
+
+import sys
+import urllib.request
+if __name__ == "__main__" :
+    data = {
+        'email': sys.argv[2]
+    }
+    #data = data.encode('ascii')
+    url = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print('Your email is: {}'.format(body.decode('utf-8')))
+
