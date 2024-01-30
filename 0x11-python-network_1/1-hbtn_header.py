b90823ed69f23ed833b112cb1fd45c4dcc281ec6
frank chege <francischege602@gmail.com>
2024-01-30 23:36:10 +0300

    web server

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,7 +5,8 @@ get the request id
 
 import sys
 import urllib.request
-url = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print('{}'.format(response.headers.get('X-Request-Id')))
+if __name__ == "__main__":
+    url = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print('{}'.format(response.headers.get('X-Request-Id')))
