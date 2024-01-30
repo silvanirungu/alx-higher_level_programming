f9b0efade2f56da1789140811c42862412a24460
frank chege <francischege602@gmail.com>
2024-01-31 02:01:21 +0300

    web server

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,13 +5,13 @@ handle http error
 
 import urllib.request
 import sys
-import urllib.error
+from  urllib.error import HTTPError
 if __name__ == '__main__':
     url = urllib.request.Request(sys.argv[1])
     try:
         with urllib.request.urlopen(url) as response:
             body = response.read()
-    except urllib.HTTPError as error:
-        print('Error code: {}'.format(urllib.error.error.code))
+    except HTTPError as err:
+        print('Error code: {}'.format(err.code))
     else:
         print(body.decode('utf-8'))
