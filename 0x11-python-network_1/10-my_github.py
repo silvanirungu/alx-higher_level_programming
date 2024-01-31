b25e1e892b9130e96d8b9028543474f7d430b29a
frank chege <francischege602@gmail.com>
2024-02-01 00:44:25 +0300

    web server

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" get the id of my github account"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    body = response.json()
+    print(body['id'])
