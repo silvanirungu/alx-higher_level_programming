1af95b45169c8d77196e3df17aec6bbc527cb653
Silvan <111249215+silvanirungu@users.noreply.github.com>
2024-03-23 19:57:17 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,17 @@
 #!/usr/bin/node
+
+// import the mode
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+
+// The first argument is the file path
+const file = process.argv[2];
+
+// The second argument is the string to write
+const content = process.argv[3];
+
+// write to file
+fs.writeFile(file, content, 'utf-8', error => {
+  if (error) {
+    console.log(error);
+  }
 });
