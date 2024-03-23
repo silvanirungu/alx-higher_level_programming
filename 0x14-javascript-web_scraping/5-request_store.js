a1cf6cdccd4eaf66523fac3ff41e2d30288be76a
Silvan <111249215+silvanirungu@users.noreply.github.com>
2024-03-23 20:00:28 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,25 @@
 #!/usr/bin/node
-const fs = require('fs');
+
+// import the module
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const fs = require('fs');
+
+// The first argument is the URL to request
+const apiUrl = process.argv[2];
+
+// The second argument the file path to store the body response
+const file = process.argv[3];
+
+// Make an HTTP GET request to the specified URL
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    // write response to file
+    fs.writeFile(file, body, 'utf-8', function (error, data) {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+});
