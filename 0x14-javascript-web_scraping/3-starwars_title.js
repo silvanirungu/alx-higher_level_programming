3ef680f3f377f98af977b34eade280262b57d7e5
Silvan <111249215+silvanirungu@users.noreply.github.com>
2024-03-23 19:59:23 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,20 @@
 #!/usr/bin/node
+
+// import the module
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+// The first argument is the movie ID
+const movieId = process.argv[2];
+
+// Construct the API URL with the provided movie ID
+const apiurl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+// Make an HTTP GET request to the API
+request(apiurl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
 });
