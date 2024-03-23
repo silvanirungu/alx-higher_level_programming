e002715c360515ec360212ca1e449f91c63e5162
Silvan <111249215+silvanirungu@users.noreply.github.com>
2024-03-23 19:59:57 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,23 @@
 #!/usr/bin/node
+
+// import the module
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
+
+// The first argument is the API URL
+const apiUrl = process.argv[2];
+
+// Make an HTTP GET request to the API
+request(apiUrl, function (error, response, body) {
   if (!error) {
+    // parse the JSON response
     const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
+
+    // count no of movies where "Wedge Antilles" is present
+    const moviesWithWedge = results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+        ? count + 1 // if found
+        : count; // if not found
+    }, 0); // initiliazing count to zero
+    console.log(moviesWithWedge);
   }
 });
