0b2488a155aa1f34facb707d5dfc6b6c5fe2ab1d
Silvan <111249215+silvanirungu@users.noreply.github.com>
2024-03-23 20:01:03 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,28 @@
 #!/usr/bin/node
+
+// import the module
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+
+// The first argument is the API URL
+const apiUrl = process.argv[2];
+
+const dictionary = {};
+
+// Make an HTTP GET request to the API URL
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
+        }
+        dictionary[userid] += 1;
       }
     });
-    console.log(completed);
+    console.log(dictionary);
   }
 });
